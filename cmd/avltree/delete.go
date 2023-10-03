52a586d58514802c47d74b57a61da77f630edb97
Rafael <rjribeiro@inf.ufrgs.br>
2023-10-03 20:03:40 -0300

    created and tested avl delete

--- cmd/avltree/delete.go
+++ cmd/avltree/delete.go
@@ -1,13 +1,109 @@
 package avltree
 
-import "cmp"
+import (
+	"cmp"
+	"math"
+)
+
+// Delete
+// It deletes the value in the tree.
+// If the tree is nil, it returns nil.
+// If the value is equal to the root, it deletes the root.
+// If the value is less than the root, it deletes in the left subtree.
+// If the value is greater than the root, it deletes in the right subtree.
+// After deleting, it rebalances the tree.
+// It returns the new root of the tree.
+func Delete[T cmp.Ordered](tree *AvlTree[T], value T) *AvlTree[T] {
+	if tree == nil {
+		return nil
+	}
+	if value == tree.value {
+		tree = deleteHelper(tree)
+		return tree
+	}
+
+	if value < tree.value {
+		tree.leftNode = Delete(tree.leftNode, value)
+	} else {
+		tree.rightNode = Delete(tree.rightNode, value)
+	}
+	tree.height = int64(math.Max(float64(getHeight(tree.leftNode)), float64(getHeight(tree.rightNode))) + 1)
+	return tree
+}
+
+// deleteHelper
+// Remove the root of the tree.
+// If the tree is a leaf, it returns nil.
+// If the tree has only one son, it returns the son.
+// If the tree has two sons, it returns the value smallest son on the right as root of a balanced tree.
+func deleteHelper[T cmp.Ordered](tree *AvlTree[T]) *AvlTree[T] {
+	if tree.leftNode == nil && tree.rightNode == nil {
+		return nil
+	}
+	if tree.leftNode != nil && tree.rightNode == nil {
+		return tree.leftNode
+	}
+	if tree.leftNode == nil && tree.rightNode != nil {
+		return tree.rightNode
+	}
+	newRootTree := getSmallestSonOnTheRight(tree)
+	tree = Delete(tree, newRootTree.value)
+	newRootTree.leftNode = tree.leftNode
+	newRootTree.rightNode = tree.rightNode
+	newRootTree.height = int64(math.Max(float64(getHeight(tree.leftNode)), float64(getHeight(tree.rightNode))) + 1)
+
+	balancedTree := getBalancedTreeAfterDelete(newRootTree)
+	return balancedTree
+}
+
+// getBalancedTreeAfterDelete
+// If tree is balanced or nil,
+//
+//	it returns tree.
+//
+// If tree is unbalanced to the left and his son on the left have greater depth on the left,
+//
+//	it returns the tree with a left rotation.
+//
+// If tree is unbalanced to the left and his son on the left have greater depth on the right,
+//
+//	it returns the tree with a double left rotation.
+//
+// If tree is unbalanced to the right and his son on the right have greater depth on the right,
+//
+//	it returns the tree with a right rotation.
+//
+// If tree is unbalanced to right and his son on the right have greater depth on the left,
+//
+//	it returns the tree with a double right rotation.
+func getBalancedTreeAfterDelete[T cmp.Ordered](tree *AvlTree[T]) *AvlTree[T] {
+	if tree == nil {
+		return tree
+	}
+	balanceFactor := getBalanceFactor(tree)
+	if balanceFactor > 1 && getBalanceFactor(tree.leftNode) == 1 {
+		return rotateLeft(tree)
+	}
+	if balanceFactor > 1 && getBalanceFactor(tree.leftNode) == -1 {
+		tree.leftNode = rotateRight(tree.leftNode)
+		return rotateLeft(tree)
+	}
+	if balanceFactor < -1 && getBalanceFactor(tree.rightNode) == -1 {
+		return rotateRight(tree)
+	}
+	if balanceFactor < -1 && getBalanceFactor(tree.rightNode) == 1 {
+		tree.rightNode = rotateLeft(tree.rightNode)
+		return rotateRight(tree)
+	}
+	return tree
+}
 
 // getSmallestSonOnTheRight
 // It returns the smallest son on the right of the tree.
 // It's assumed that the root of the tree is not nil and has a right son at the first level
-func getSmallestSonOnTheRight[T cmp.Ordered](tree *AvlTree[T]) T {
+func getSmallestSonOnTheRight[T cmp.Ordered](tree *AvlTree[T]) *AvlTree[T] {
 	smallestNode := tree.rightNode
 	for ; smallestNode.leftNode != nil; smallestNode = smallestNode.leftNode {
 	}
-	return smallestNode.value
+	return smallestNode
 }
--- cmd/avltree/delete_test.go
+++ cmd/avltree/delete_test.go
@@ -8,22 +8,13 @@ import (
 func TestGetSmallestSonOnTheRight(t *testing.T) {
 	testCases := []struct {
 		input  *AvlTree[int]
-		output int
+		output *AvlTree[int]
 	}{
 		{
 			input: &AvlTree[int]{value: 8, height: 1,
 				rightNode: &AvlTree[int]{value: 10, height: 0},
 			},
-			output: 10,
-		},
-		{
-			input: &AvlTree[int]{value: 8, height: 2,
-				leftNode: &AvlTree[int]{value: 5, height: 0},
-				rightNode: &AvlTree[int]{value: 10, height: 1,
-					leftNode: &AvlTree[int]{value: 9, height: 0},
-				},
-			},
-			output: 9,
+			output: &AvlTree[int]{value: 10, height: 0},
 		},
 		{
 			input: &AvlTree[int]{value: 8, height: 2,
@@ -32,21 +23,234 @@ func TestGetSmallestSonOnTheRight(t *testing.T) {
 				},
 				rightNode: &AvlTree[int]{value: 10, height: 0},
 			},
-			output: 10,
+			output: &AvlTree[int]{value: 10, height: 0},
 		},
 		{
-			input: &AvlTree[int]{value: 8, height: 2,
-				leftNode: &AvlTree[int]{value: 5, height: 1},
-				rightNode: &AvlTree[int]{value: 10, height: 1,
-					rightNode: &AvlTree[int]{value: 12, height: 0},
+			input: &AvlTree[int]{value: 5, height: 2,
+				leftNode: &AvlTree[int]{value: 2, height: 1,
+					leftNode: &AvlTree[int]{value: 1, height: 0},
+				},
+				rightNode: &AvlTree[int]{value: 10, height: 2,
+					leftNode: &AvlTree[int]{value: 7, height: 1,
+						leftNode: &AvlTree[int]{value: 6, height: 0},
+					},
+					rightNode: &AvlTree[int]{value: 12, height: 1},
 				},
 			},
-			output: 10,
+			output: &AvlTree[int]{value: 6, height: 0},
 		},
 	}
 
 	for index, testCase := range testCases {
 		outputReturned := getSmallestSonOnTheRight(testCase.input)
+		if !reflect.DeepEqual(outputReturned, testCase.output) {
+			t.Errorf("Return case %d\ngot: \n%v\n\nwant: \n%v", index, outputReturned, testCase.output)
+		}
+	}
+}
+
+func TestDeleteHelper(t *testing.T) {
+	testCases := []struct {
+		input  *AvlTree[int]
+		output *AvlTree[int]
+	}{
+		{ // leaf
+			input:  &AvlTree[int]{value: 8, height: 0},
+			output: nil,
+		},
+		{
+			// one son on the left
+			input: &AvlTree[int]{value: 8, height: 1,
+				leftNode: &AvlTree[int]{value: 5, height: 0},
+			},
+			output: &AvlTree[int]{value: 5, height: 0},
+		},
+		{
+			// one son on the right
+			input: &AvlTree[int]{value: 8, height: 1,
+				rightNode: &AvlTree[int]{value: 10, height: 0},
+			},
+			output: &AvlTree[int]{value: 10, height: 0},
+		},
+		{
+			// two sons
+			input: &AvlTree[int]{value: 8, height: 2,
+				leftNode: &AvlTree[int]{value: 5, height: 1,
+					leftNode: &AvlTree[int]{value: 3, height: 0},
+				},
+				rightNode: &AvlTree[int]{value: 10, height: 0},
+			},
+			output: &AvlTree[int]{value: 5, height: 1,
+				leftNode:  &AvlTree[int]{value: 3, height: 0},
+				rightNode: &AvlTree[int]{value: 10, height: 0},
+			},
+		},
+		{
+			// two sons
+			input: &AvlTree[int]{value: 8, height: 2,
+				leftNode: &AvlTree[int]{value: 5, height: 1,
+					leftNode: &AvlTree[int]{value: 3, height: 0},
+				},
+				rightNode: &AvlTree[int]{value: 10, height: 1,
+					leftNode: &AvlTree[int]{value: 9, height: 0},
+				},
+			},
+			output: &AvlTree[int]{value: 9, height: 2,
+				leftNode: &AvlTree[int]{value: 5, height: 1,
+					leftNode: &AvlTree[int]{value: 3, height: 0},
+				},
+				rightNode: &AvlTree[int]{value: 10, height: 0},
+			},
+		},
+	}
+	for index, testCase := range testCases {
+		outputReturned := deleteHelper(testCase.input)
+		if !reflect.DeepEqual(outputReturned, testCase.output) {
+			t.Errorf("Return case %d\ngot: \n%v\n\nwant: \n%v", index, outputReturned, testCase.output)
+		}
+	}
+}
+
+func TestGetBalancedTreeAfterDelete(t *testing.T) {
+	testCases := []struct {
+		input  *AvlTree[int]
+		output *AvlTree[int]
+	}{
+		{ // nil
+			input:  nil,
+			output: nil,
+		},
+		{
+			// balanced tree
+			input: &AvlTree[int]{value: 8, height: 1,
+				leftNode:  &AvlTree[int]{value: 5, height: 0},
+				rightNode: &AvlTree[int]{value: 10, height: 0},
+			},
+			output: &AvlTree[int]{value: 8, height: 1,
+				leftNode:  &AvlTree[int]{value: 5, height: 0},
+				rightNode: &AvlTree[int]{value: 10, height: 0},
+			},
+		},
+		{
+			// unbalanced to the left
+			input: &AvlTree[int]{value: 8, height: 2,
+				leftNode: &AvlTree[int]{value: 5, height: 1,
+					leftNode: &AvlTree[int]{value: 3, height: 0},
+				},
+			},
+			output: &AvlTree[int]{value: 5, height: 1,
+				leftNode:  &AvlTree[int]{value: 3, height: 0},
+				rightNode: &AvlTree[int]{value: 8, height: 0},
+			},
+		},
+		{
+			// unbalanced to the left and his son on the left have greater depth on the right
+			input: &AvlTree[int]{value: 8, height: 2,
+				leftNode: &AvlTree[int]{value: 5, height: 1,
+					rightNode: &AvlTree[int]{value: 7, height: 0},
+				},
+			},
+			output: &AvlTree[int]{value: 7, height: 1,
+				leftNode:  &AvlTree[int]{value: 5, height: 0},
+				rightNode: &AvlTree[int]{value: 8, height: 0},
+			},
+		},
+		{
+			// unbalanced to the right
+			input: &AvlTree[int]{value: 8, height: 2,
+				rightNode: &AvlTree[int]{value: 10, height: 1,
+					rightNode: &AvlTree[int]{value: 12, height: 0},
+				},
+			},
+			output: &AvlTree[int]{value: 10, height: 1,
+				leftNode:  &AvlTree[int]{value: 8, height: 0},
+				rightNode: &AvlTree[int]{value: 12, height: 0},
+			},
+		},
+		{
+			// unbalanced to the right and his son on the right have greater depth on the left
+			input: &AvlTree[int]{value: 8, height: 2,
+				rightNode: &AvlTree[int]{value: 10, height: 1,
+					leftNode: &AvlTree[int]{value: 9, height: 0},
+				},
+			},
+			output: &AvlTree[int]{value: 9, height: 1,
+				leftNode:  &AvlTree[int]{value: 8, height: 0},
+				rightNode: &AvlTree[int]{value: 10, height: 0},
+			},
+		},
+	}
+	for index, testCase := range testCases {
+		outputReturned := getBalancedTreeAfterDelete(testCase.input)
+		if !reflect.DeepEqual(outputReturned, testCase.output) {
+			t.Errorf("Return case %d\ngot: \n%v\n\nwant: \n%v", index, outputReturned, testCase.output)
+		}
+	}
+}
+
+func TestDelete(t *testing.T) {
+	testCases := []struct {
+		input  *AvlTree[int]
+		value  int
+		output *AvlTree[int]
+	}{
+		{ // nil
+			input:  nil,
+			value:  8,
+			output: nil,
+		},
+		{ // leaf
+			input:  &AvlTree[int]{value: 8, height: 0},
+			value:  8,
+			output: nil,
+		},
+		{ // one son on the left
+			input: &AvlTree[int]{value: 8, height: 1,
+				leftNode: &AvlTree[int]{value: 5, height: 0},
+			},
+			value:  8,
+			output: &AvlTree[int]{value: 5, height: 0},
+		},
+		{ // one son on the right
+			input: &AvlTree[int]{value: 8, height: 1,
+				rightNode: &AvlTree[int]{value: 10, height: 0},
+			},
+			value:  8,
+			output: &AvlTree[int]{value: 10, height: 0},
+		},
+		{ // two sons
+			input: &AvlTree[int]{value: 8, height: 2,
+				leftNode: &AvlTree[int]{value: 5, height: 1,
+					leftNode: &AvlTree[int]{value: 3, height: 0},
+				},
+				rightNode: &AvlTree[int]{value: 10, height: 0},
+			},
+			value: 8,
+			output: &AvlTree[int]{value: 5, height: 1,
+				leftNode:  &AvlTree[int]{value: 3, height: 0},
+				rightNode: &AvlTree[int]{value: 10, height: 0},
+			},
+		},
+		{ // two sons, value on the middle
+			input: &AvlTree[int]{value: 8, height: 2,
+				leftNode: &AvlTree[int]{value: 5, height: 1,
+					leftNode: &AvlTree[int]{value: 3, height: 0},
+				},
+				rightNode: &AvlTree[int]{value: 10, height: 1,
+					leftNode: &AvlTree[int]{value: 9, height: 0},
+				},
+			},
+			value: 5,
+			output: &AvlTree[int]{value: 8, height: 2,
+				leftNode: &AvlTree[int]{value: 3, height: 0},
+				rightNode: &AvlTree[int]{value: 10, height: 1,
+					leftNode: &AvlTree[int]{value: 9, height: 0},
+				},
+			},
+		},
+	}
+	for index, testCase := range testCases {
+		outputReturned := Delete(testCase.input, testCase.value)
 		if !reflect.DeepEqual(outputReturned, testCase.output) {
 			t.Errorf("Return case %d\ngot: \n%v\n\nwant: \n%v", index, outputReturned, testCase.output)
 		}
