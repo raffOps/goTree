b7de3ac1b00b5aabea8d6d822c09a489afb71f99
Rafael <rjribeiro@inf.ufrgs.br>
2023-10-03 21:09:25 -0300

    fixed avl rotations

--- cmd/avltree/rotate.go
+++ cmd/avltree/rotate.go
@@ -5,22 +5,9 @@ import (
 	"math"
 )
 
-// RotationNotPossible is raised when the tree is nil or when the left/right son of the tree is nil.
-type RotationNotPossible struct {
-	Message string
-}
-
-func (e *RotationNotPossible) Error() string {
-	return e.Message
-}
-
 // rotateLeft performs a left rotation on the tree. It returns the new root of the tree.
-// It panics if the tree is nil or if the left son of the tree is nil.
+// It's assumed that the tree is nil or if the left son of the tree is nil.
 func rotateLeft[T cmp.Ordered](tree *AvlTree[T]) *AvlTree[T] {
-	if tree == nil || tree.leftNode == nil {
-		err := RotationNotPossible{"Tree or left son is nil"}
-		panic(err)
-	}
 	rotatedTree := tree.leftNode
 	tree.leftNode = rotatedTree.rightNode
 	rotatedTree.rightNode = tree
@@ -31,12 +18,8 @@ func rotateLeft[T cmp.Ordered](tree *AvlTree[T]) *AvlTree[T] {
 }
 
 // rotateRight performs a right rotation on the tree. It returns the new root of the tree.
-// It panics if the tree is nil or if the right son of the tree is nil.
+// It's assumed that tree is nil or if the right son of the tree is nil.
 func rotateRight[T cmp.Ordered](tree *AvlTree[T]) *AvlTree[T] {
-	if tree == nil || tree.rightNode == nil {
-		err := RotationNotPossible{"Tree and/or right son is nil"}
-		panic(err)
-	}
 	rotatedTree := tree.rightNode
 	tree.rightNode = rotatedTree.leftNode
 	rotatedTree.leftNode = tree
