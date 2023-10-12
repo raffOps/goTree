66deb9d665791e6ab355235d5670a7013825532d
Rafael <rjribeiro@inf.ufrgs.br>
2023-10-12 15:50:43 -0300

    feat: BinaryTee common methods

--- cmd/binarytree/binarytree.go
+++ cmd/binarytree/binarytree.go
@@ -4,6 +4,12 @@ import (
 	"cmp"
 )
 
+// BinaryTree is an interface for binary tree
+type BinaryTree[T cmp.Ordered] interface {
+	GetValue() T
+	GetChild(direction string) *BinaryTree[T]
+}
+
 // SelfBalancingBinaryTree is an interface for self-balancing binary tree
 type SelfBalancingBinaryTree[T cmp.Ordered] interface {
 	InsertWithSelfBalancing(value T)
