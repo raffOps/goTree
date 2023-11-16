d79b3e2b6bfc2839b458143782a57e0a981c6669
Rafael <rjribeiro@inf.ufrgs.br>
2023-11-16 19:49:32 -0300

    feat: rb insert tests

--- cmd/redblacktree/insert.go
+++ cmd/redblacktree/insert.go
@@ -1,1 +1,22 @@
 package redblacktree
+
+import "cmp"
+
+// getRecoloredTree
+func getRecoloredTree[T cmp.Ordered](tree *RedBlackTree[T]) *RedBlackTree[T] {
+	tree.isRed = !tree.isRed
+	tree.leftNode.isRed = !tree.leftNode.isRed
+	tree.rightNode.isRed = !tree.rightNode.isRed
+	return tree
+}
+
+// Insert a value into the tree.
+// If the tree is nil, it returns a new tree with the value.
+// If the value is already in the tree, it returns the tree.
+// If the value is less than the root value, it inserts the value in the left subtree.
+// If the value is greater than the root value, it inserts the value in the right subtree.
+// After inserting, it rebalances the tree.
+// It returns the new root of the tree.
+func Insert[T cmp.Ordered](tree *RedBlackTree[T], value T) *RedBlackTree[T] {
+	return tree
+}
---
+++ cmd/redblacktree/insert_test.go
@@ -0,0 +1,208 @@
+package redblacktree
+
+import (
+	"cmp"
+	"reflect"
+	"testing"
+)
+
+type getRecoloredTestCase[T cmp.Ordered] struct {
+	tree *RedBlackTree[T]
+	want *RedBlackTree[T]
+}
+
+func TestGetRecoloredTree(t *testing.T) {
+	testCases := getRecoloredTreeTestCases()
+
+	for index, testCase := range testCases {
+		getRecoloredTree(testCase.tree)
+		if !reflect.DeepEqual(testCase.tree, testCase.want) {
+			t.Errorf("test case %v: got \n%v \nwant \n%v", index, testCase.tree, testCase.want)
+		}
+	}
+}
+
+func getRecoloredTreeTestCases() []getRecoloredTestCase[int] {
+	var testCases []getRecoloredTestCase[int]
+
+	// tree
+	node1 := &RedBlackTree[int]{
+		value: &nodeValue[int]{1},
+		isRed: true,
+	}
+
+	node3 := &RedBlackTree[int]{
+		value: &nodeValue[int]{3},
+		isRed: true,
+	}
+
+	node2 := &RedBlackTree[int]{
+		value:     &nodeValue[int]{2},
+		leftNode:  node1,
+		rightNode: node3,
+		isRed:     false,
+	}
+
+	// want
+
+	wantNode1 := &RedBlackTree[int]{
+		value: &nodeValue[int]{1},
+		isRed: false,
+	}
+
+	wantNode3 := &RedBlackTree[int]{
+		value: &nodeValue[int]{3},
+		isRed: false,
+	}
+
+	wantNode2 := &RedBlackTree[int]{
+		value:     &nodeValue[int]{2},
+		leftNode:  wantNode1,
+		rightNode: wantNode3,
+		isRed:     true,
+	}
+
+	testCases = append(testCases, getRecoloredTestCase[int]{node2, wantNode2})
+	return testCases
+}
+
+type insertTestCase[T cmp.Ordered] struct {
+	tree  *RedBlackTree[T]
+	value T
+	want  *RedBlackTree[T]
+}
+
+func TestInsert(t *testing.T) {
+	testCases := getInsertTestCases()
+
+	for index, testCase := range testCases {
+		Insert(testCase.tree, testCase.value)
+		if !reflect.DeepEqual(testCase.tree, testCase.want) {
+			t.Errorf("test case %v: got \n%v \nwant \n%v", index, testCase.tree, testCase.want)
+		}
+	}
+}
+
+func getInsertTestCases() (testCases []insertTestCase[int]) {
+
+	var tree, want *RedBlackTree[int]
+	var value int
+
+	// test case 1: empty tree
+	tree = &RedBlackTree[int]{}
+	value = 3
+	want = &RedBlackTree[int]{
+		value: &nodeValue[int]{value: 3},
+		isRed: false,
+	}
+	testCases = append(testCases, insertTestCase[int]{tree, value, want})
+
+	// test case 2: value lesser than root value.
+	tree = &RedBlackTree[int]{
+		value: &nodeValue[int]{value: 3},
+	}
+	value = 2
+	want = &RedBlackTree[int]{
+		value: &nodeValue[int]{value: 3},
+		leftNode: &RedBlackTree[int]{
+			value: &nodeValue[int]{value: 2},
+			isRed: true,
+		},
+	}
+
+	testCases = append(testCases, insertTestCase[int]{tree, value, want})
+
+	// test case 3: value greater than root value.
+	tree = &RedBlackTree[int]{
+		value: &nodeValue[int]{value: 3},
+	}
+	value = 4
+	want = &RedBlackTree[int]{
+		value: &nodeValue[int]{value: 4},
+		leftNode: &RedBlackTree[int]{
+			value: &nodeValue[int]{value: 3},
+			isRed: true,
+		},
+	}
+
+	testCases = append(testCases, insertTestCase[int]{tree, value, want})
+
+	// test case 4: value greater than root. Root with left child.
+
+	tree = &RedBlackTree[int]{
+		value: &nodeValue[int]{value: 3},
+		leftNode: &RedBlackTree[int]{
+			value: &nodeValue[int]{value: 2},
+			isRed: true,
+		},
+	}
+
+	value = 4
+	want = &RedBlackTree[int]{
+		value: &nodeValue[int]{value: 3},
+		isRed: false,
+		leftNode: &RedBlackTree[int]{
+			value: &nodeValue[int]{value: 2},
+			isRed: false,
+		},
+		rightNode: &RedBlackTree[int]{
+			value: &nodeValue[int]{value: 4},
+			isRed: false,
+		},
+	}
+
+	testCases = append(testCases, insertTestCase[int]{tree, value, want})
+
+	// test case 5: value lesser than left son.
+
+	tree = &RedBlackTree[int]{
+		value: &nodeValue[int]{value: 3},
+		leftNode: &RedBlackTree[int]{
+			value: &nodeValue[int]{value: 2},
+			isRed: true,
+		},
+	}
+
+	value = 1
+	want = &RedBlackTree[int]{
+		value: &nodeValue[int]{value: 2},
+		isRed: false,
+		leftNode: &RedBlackTree[int]{
+			value: &nodeValue[int]{value: 1},
+			isRed: false,
+		},
+		rightNode: &RedBlackTree[int]{
+			value: &nodeValue[int]{value: 3},
+			isRed: false,
+		},
+	}
+
+	testCases = append(testCases, insertTestCase[int]{tree, value, want})
+
+	// test case 6: value greater than left son, lesser than root.
+
+	tree = &RedBlackTree[int]{
+		value: &nodeValue[int]{value: 3},
+		leftNode: &RedBlackTree[int]{
+			value: &nodeValue[int]{value: 1},
+			isRed: true,
+		},
+	}
+	value = 2
+	want = &RedBlackTree[int]{
+		value: &nodeValue[int]{value: 2},
+		isRed: false,
+		leftNode: &RedBlackTree[int]{
+			value: &nodeValue[int]{value: 1},
+			isRed: false,
+		},
+		rightNode: &RedBlackTree[int]{
+			value: &nodeValue[int]{value: 3},
+			isRed: false,
+		},
+	}
+
+	testCases = append(testCases, insertTestCase[int]{tree, value, want})
+
+	return testCases
+}
